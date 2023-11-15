d9b7b553f6a51c2f8dcba5a7659ac98a0011215d
Stephane Humblot <SHumblot@oph74.fr>
2023-11-15 10:38:30 +0100

    feat(animatedcard): using gsap to animate, new syntax

--- src/animations.ts
+++ src/animations.ts
@@ -1,23 +1,37 @@
+import { gsap } from "gsap";
+import { TweenMax, Bounce } from "gsap/all";
+
+gsap.registerPlugin(TweenMax, Bounce);
+
 interface Animations {
-  [key: string]: (e: Event) => void
+  [key: string]: (target: HTMLElement) => gsap.core.Tween[];
 }
 
-function flipCard (e: Event) {
-  alert('TODO')
+function rotate (target: HTMLElement) {
+  return [
+    gsap.to(target, {duration: 2, rotation: 360}),
+    gsap.to(target, {duration: 0, rotation: 0, delay: 2})
+  ]
 }
 
-function bounce (e: Event) {
-  alert('TODO')
+function flip (target: HTMLElement) {
+  return [
+    gsap.to(target, {rotationY: 360, duration: 2}),
+    gsap.to(target, {rotationY: 0, duration: 0, delay: 2})
+  ]
 }
 
-function turn (e: Event) {
-  alert('TODO')
+function bounce (target: HTMLElement) {
+  return [
+    gsap.to(target, {duration: 0.4, scale: 1.5, ease: "bounce.easeOut"}),
+    gsap.to(target, {duration: 0.2, scale: 1, delay: 0.4})
+  ]
 }
 
 const animations : Animations = {
-  flipCard,
+  rotate,
   bounce,
-  turn,
+  flip
 }
 
 export default animations;
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,93 @@
-import { LitElement} from "lit";
+import {css, LitElement} from "lit";
 import {customElement, property} from 'lit/decorators.js';
 import animations from "./animations";
 
-
-function addListener(target: HTMLElement, on: string , execute: (e: Event) => void): void {
-    target.addEventListener(on, (e) => execute(e));
+function getDomTarget(targetName: string): Element|Window|Document|undefined|null {
+  let target;
+  if (targetName === 'document') {
+      target = document;
+    } else if (targetName === 'window') {
+    target = window;
+    } else {
+    target = document.querySelector(targetName);
+    }
+  return target;
 }
 
-function createCard(on: string, execute: (e: Event ) => void): HTMLDivElement {
+function addListener(
+  target: HTMLElement,
+  animationsToTrigger: string,
+  refs: {[key: string]: gsap.core.Tween[]},
+  isRunning: {[key:string]: boolean}
+): void {
+
+  animationsToTrigger.split(',').forEach((animationToTrigger: string) => {
+    const [animation, on, domTargetName] = animationToTrigger.trim().split(':')
+    if (on === undefined || animation === undefined) {
+      console.error("Your animation definition is wrong. It should be like 'rotate:scroll:document' or 'click:flip', ...")
+      return
+    }
+
+    if (animations[animation] === undefined) {
+      console.error(`The animation ${animation} is not defined. Please check your spelling.`)
+      return;
+    }
+
+    let domTarget;
+    if (domTargetName !== undefined) {
+      domTarget = getDomTarget(domTargetName);
+      if (!domTarget) {
+        console.error(`The target "${domTargetName}" is not defined. Please check your spelling.`)
+        return;
+      }
+    }
+
+    (domTarget ? domTarget : target).addEventListener(on, () => {
+      isRunning[animationToTrigger] = refs[animationToTrigger] ?
+        refs[animationToTrigger].some((gsapAnimation: gsap.core.Tween) => gsapAnimation.isActive())
+        : false;
+      if (isRunning[animationToTrigger]) return
+      refs[animationToTrigger] = animations[animation](target);
+    });
+  });
+}
+
+function createCard(
+  animationsToTrigger: string,
+  refs: gsap.core.Tween[],
+  isRunning: {[key:string]: boolean}
+): HTMLDivElement {
   const cardDiv = document.createElement('div');
-  cardDiv.className = 'card';
-  cardDiv.innerHTML = '<slot></slot>'
-  addListener(cardDiv, on, execute)
+  cardDiv.classList.add('card');
+  cardDiv.innerHTML = '<slot></slot>';
+  addListener(cardDiv, animationsToTrigger, refs, isRunning);
   return cardDiv;
 }
 
 @customElement('animated-card')
 export class AnimatedCard extends LitElement {
   @property({type: String})
-  on?: string;
+  animations: string = '';
 
-  @property({type: String})
-  animation?: string;
+  animationsRef: {[key: string]: gsap.core.Tween[]} = {};
+  isRunning: {[key:string]: boolean} = {};
 
-  cardDiv : HTMLDivElement= document.createElement('div');
+  static styles =  [
+    css`
+    .card {
+      width: fit-content;
+      padding: 10px;
+      border: solid 1px grey;
+      border-radius: 10px;
+    }`]
 
   protected render(): HTMLDivElement|string {
-    if (this.on === undefined || this.animation === undefined) {
-      console.error('You have to define a trigger and an animation to use animated cards')
+    if (this.animations === undefined) {
+      console.error('You have to define an animation to use animated cards')
       return '';
     }
-
-    return createCard(this.on, animations[this.animation])
+    console.log(this.animations)
+    return createCard(this.animations, this.animationsRef, this.isRunning)
   }
 }
 
@@ -40,3 +96,5 @@ declare global {
     "animated-card": AnimatedCard;
   }
 }
+
+export default AnimatedCard;
